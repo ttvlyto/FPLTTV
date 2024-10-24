5d93638c6880c3a2cbb604a6b5a544497efd1ec7
ttvlyto <124396529+ttvlyto@users.noreply.github.com>
2024-10-24 12:29:08 -0700

    tried to clean, make parser more usable, less reliant on txt

--- StatCrawler.py
+++ StatCrawler.py
@@ -1,8 +1,15 @@
 import asyncio
 from playwright.sync_api import sync_playwright
 import time
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+import parserS
 async def cookies():
     await page.click("#onetrust-accept-btn-handler")
+    closeAd = page.get_by_role("link", name="Close Advert")
+    await closeAd.wait_for(state="visable", timeout=30000)
+    await closeAd.click()
+
+
 
 stats = []
 html = []
@@ -16,12 +23,18 @@ with sync_playwright() as p: ##launches browser using chromium
         link = "https://" + stats[x]
         page.goto(link)
         context = browser.new_context()
-        cookies()
+        try:
+            page.click("#onetrust-accept-btn-handler", timeout=100)
+        except PlaywrightTimeoutError:
+            print("No cookies")
+
+
+
+
         html.append(page.content())
         time.sleep(2)
     browser.close()
+    len = len(html)
 
-    f = open("stats.txt", "w")
-    f.write(html[0])
-    f.close()
+parserS.parseStats(html, len)
 
--- parserS.py
+++ parserS.py
@@ -1,20 +1,22 @@
 from bs4 import BeautifulSoup
 import asyncio
 f = open("stats.txt", "r")
-for line in f:
-    newstr = f.read()
-    break
+def parseStats(stats, len):
 
-soup = BeautifulSoup(newstr, "html.parser")
-data = soup.find_all("div", {"class":"player-overview__info"})
-moreData = soup.find_all("div", {"data-script":"pl_player"})
-evenMoreData = soup.find("div", {"data-widget":"player-overview-stats"})
-for x in data:
-    print(x.get_text())
+    for x in range(0,len):
+        newstr = stats[x]
+        soup = BeautifulSoup(newstr, "html.parser")
+        data = soup.find_all("div", {"class": "player-overview__info"})
+        moreData = soup.find_all("div", {"data-script": "pl_player"})
+        evenMoreData = soup.find("div", {"data-widget": "player-overview-stats"})
+        for x in data:
+            print(x.get_text())
 
-print("end ******")
-for x in moreData:
-    print(x)
+        print("end ******")
+        for x in moreData:
+            print(x.get_text())
 
-print("end ******")
-evenMoreData.getChild
+        print("end ******")
+
+
+    ##evenMoreData.getChild
