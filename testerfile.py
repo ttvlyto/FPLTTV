78fe3bfb6e582ead03c867daceb29ea9eb226e07
lyto <mjimenez3630@sdsu.edu>
2024-11-07 09:09:17 -0800

    hopefully committing a testing portion of the code

---
+++ testerfile.py
@@ -0,0 +1,25 @@
+
+f = open("playerStats.txt", 'r')
+strlen = len("overview__info")
+players = open("links.txt")
+
+print(strlen)
+x = 0
+
+for line in f:
+    newstr = line
+    x += 1
+    i = 0
+    if x==2830:
+        print("something")
+    if newstr == '******end, new player******\n':
+        print('new player has been found, reset')
+    while i < len(newstr):
+        if newstr[i:i+14] == "overview__info":
+            newstr = next(f)
+            print(newstr.replace(" ", ""))
+
+
+        else:
+            i += 1
+
